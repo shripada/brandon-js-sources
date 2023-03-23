ecac75a30f58127bb7f3ce9c16d1237f52c5fea2
Shripada Hebbar <shripada.hebbar@codecraft.co.in>
2023-03-23 09:55:20 +0530

    seperated tests from the implementation of prime functions

--- prime.js
+++ prime.js
@@ -61,7 +61,6 @@ function isPrime(number) {
     assert(typeof number === 'number', "argument must be a number");
 
     let isNumberPrime = true;
-
     let numberValue = parseInt(number);
     if( numberValue === 0 || numberValue === 1){
      isNumberPrime = false;
@@ -70,7 +69,7 @@ function isPrime(number) {
       return true;
     }
  
-    for(let i = 2; i <= Math.sqrt(numberValue); i++){
+    for(let i = 2; i <= number/2; i++){
        if(numberValue % i === 0) { // reminder is 0 means, i is a factor of number. So number is not a prime
          isNumberPrime = false; 
          break;
@@ -79,33 +78,6 @@ function isPrime(number) {
 
     return isNumberPrime;
 }
-
-// Welcome asserts
-// asserts help us to specify the invariants in a function.
-let prime = isPrime(10);
-assert(prime === false, "10 is not a prime number");
-
-prime = isPrime(0);
-assert(prime === false, "0 is not a prime number");
-
-prime = isPrime(1);
-assert(prime === false, "1 is not a prime number");
-
-prime = isPrime(2)
-assert(prime === true, "2 is a prime number");
-
-prime = isPrime(4)
-assert(prime === false, "4 is  not a prime number");
-
-prime = isPrime(997)
-assert(prime === true, "997 is a prime number");
-prime = isPrime(11317);
-assert(prime === true, "11317 is a prime number");
-//prime = isPrime({});  // We expect this to throw an error 
-assert.throws(() => {isPrime({})}, "Passing a non number value must throw");
-assert.throws(() => {isPrime("")}, "Passing a non number value must throw");
-assert.throws(() => isPrime(), "Passing a non number value must throw");
-assert.throws(function (){isPrime()}, "Passing a non number value must throw");
 
 
 /**
@@ -132,35 +104,4 @@ function generatePrimes(count){
   return primes;
 }
 
-// Tests for generatePrimes
-// count 0
-let expected = [] // Arrays use pass by reference semantics
-let actual = generatePrimes(0);
-assert.deepEqual(actual, expected,  "If the count is 0, we must get an empty array as result");
-
-// count 1
- expected = [2] // Arrays use pass by reference semantics
- actual = generatePrimes(1);
-assert.deepEqual(actual, expected,  "If the count is 1, we must get [2]");
-
-// prime series : 2, 3, 5, 7, 11, 13, 17, 19...
-// count 2
- expected = [2, 3] // Arrays use pass by reference semantics
- actual = generatePrimes(2);
-assert.deepEqual(actual, expected,  "If the count is 2, we must get [2, 3]");
-
-// count 3
- expected = [2, 3, 5] // Arrays use pass by reference semantics
- actual = generatePrimes(3);
-assert.deepEqual(actual, expected,  "If the count is 3, we must get [2, 3, 5]");
-
-// count 8
- expected = [2, 3, 5, 7, 11, 13, 17, 19] // Arrays use pass by reference semantics
- actual = generatePrimes(8);
-assert.deepEqual(actual, expected,  "If the count is 8, we must get [2, 3, 5, 7, 11, 13, 17, 19]");
-
-expected = [2,  3,  5,  7, 11, 13, 17,
-  19, 23, 29, 31, 37, 41, 43,
-  47, 53, 59, 61, 67, 71];
-actual = generatePrimes(20);
-assert.deepEqual(actual, expected,  "If the count is 20, we must get 20 primes");
+module.exports = { isPrime, generatePrimes}
