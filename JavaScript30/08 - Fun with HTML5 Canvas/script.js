6806aa59fa7d414ee9c64e1481fe71aed48a80f1
shripada <shripadah@yahoo.co.in>
2023-06-15 19:20:43 +0530

    added wesbos solutions done in sessions here as well

---
+++ JavaScript30/08 - Fun with HTML5 Canvas/script.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('#draw');
+
+// We are going to draw in 2D world
+const context = canvas.getContext('2d');
+
+// Context is like a set of settings that will be used for drawing.
+context.strokeStyle = '#BADA55';
+context.lineWidth = 40;
